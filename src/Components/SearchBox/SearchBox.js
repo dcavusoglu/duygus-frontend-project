f9c0db9fce14a4efa82136e1765a86847c014a78
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2022-09-06 08:13:55 +0300

    searchbox

--- src/Components/SearchBox/SearchBox.js
+++ src/Components/SearchBox/SearchBox.js
@@ -2,47 +2,60 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import GoBtn from '../Bottons/GoBtn'
 import './SearchBox.css'
-const SearchBox = () => {
 
+const initialState = { userInput: ""};
 
-  const [allProducts, setAllProducts] = useState([]);
+const SearchBox = ( {addSearchInput} ) => {
 
-  useEffect (() => {
-    const endPoint = 'https://dummyjson.com/products'
-    axios.get(endPoint)
+  const [searchInput, setSearchInput] = useState(initialState)
+  const [searchedProducts, setSearchedProducts] = useState([])
+
+  const handleSearch = (e) => {
+    //input
+    let input = (e.target.value).toLowerCase();
+    console.log('input', input)
+    setSearchInput(input);
+    console.log(searchInput)
+
+    axios.get(`https://dummyjson.com/products/search?q=${searchInput}`)
     .then((res) => {
-      const allProducts = res.data.products
-      console.log('All:', res.data.products.map(product => product.images.map(img =>img)))
-      setAllProducts(allProducts)
-        })
+      const searchedItem = res.data.products;
+      console.log(searchedItem)
+      setSearchedProducts(searchedItem);
+      console.log()
+      })
         .catch(err => console.log(`Error: ${err}`))
 
-      }, [])
 
-    const handleSearch = (e) => {
-    let input = e.target.value
-    input=input.toLowerCase();
-    let x = allProducts.map(product => product.title.toLowerCase());
-    console.log(x)
-    if(x.includes(input)) {
-      console.log(input)
-      const searchedItem = input
-    }
+    // let input = e.target.value
+    // input=input.toLowerCase();
+    // let x = allProducts.map(product => product.title.toLowerCase());
+    // console.log(x)
+    // if(x.includes(input)) {
+    //   console.log(input)
+    //   const searchedItem = input
+    // }
+  }
+
+  const handleSubmit = (e) => {
     e.preventDefault();
+    addSearchInput(searchInput);
+    setSearchInput(initialState);
 
-    //
-}
-
+  }
+  console.log(searchedProducts)
 
 
   return (
     <div>
       <div className='search-box'>
+          <form onSubmit={handleSubmit}>
             <label>Search</label>
             <div className='input-go'>
               <input type="text" id="site-search" name="q" onChange={handleSearch}/>
               <GoBtn/>
             </div>
+          </form>
           </div>
     </div>
   )
